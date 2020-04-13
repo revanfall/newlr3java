da1f0d8b6a51b9522fcccc614e5bf272c754c9b4
Ilya <i.d.stolyarchuk@gmail.com>
2020-04-13 10:40:38 +0300

    Initial commit

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,95 @@
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Frontend extends AbstractHandler implements Runnable {
+
+    public AtomicInteger HandleCount = new AtomicInteger();
+    private Map<String, String> fakeAccounter = new HashMap<String, String>();
+
+    public Frontend()   {
+        fakeAccounter.put("Illia", "1");
+        fakeAccounter.put("Igor", "2");
+        fakeAccounter.put("Cristiano", "3");
+        fakeAccounter.put("Denis", "4");
+        fakeAccounter.put("Vova", "5");
+        fakeAccounter.put("Maxon", "6");
+    }
+
+
+
+    public void handle(String urlPath,
+                       Request request,
+                       HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse)throws IOException, ServletException {
+        httpServletResponse.setContentType("text/html;charset=utf-8");
+        String userId, username;
+        PrintWriter pageWriter = httpServletResponse.getWriter();
+
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        request.setHandled(true);
+        HttpSession session = request.getSession();
+        if (session.getAttribute("state") == null) {
+            session.setAttribute("state", "start");
+        }
+
+        if (urlPath.equals("/") && session.getAttribute("state").equals("start")){
+            String html =
+                    "<form action=\"/login\">" +
+                            " <label>Login: <input type=\"text\" name=\"username\" required></label>" +
+                            "<input type=\"submit\" value=\"Send\">" +
+                            "   <input type=\"reset\" value=\"Clean\">"+
+                            "</form>";
+            pageWriter.println(html);
+        }
+        System.out.println("urlPath = [" + urlPath + "]");
+        if (urlPath.equals("/login") && session.getAttribute("state").equals("start")){
+            username = request.getParameter("username");
+            userId = fakeAccounter.get(username);
+            if (userId == null){
+                String html = "<div>"+ "No user is found" + "</div>" +
+                        "<a href=\"/\">Autorization</a>";
+                pageWriter.println(html);
+            } else {
+                session.setAttribute("state", "authorized");
+                session.setAttribute("userId", userId);
+                session.setAttribute("username", username);
+                httpServletResponse.sendRedirect("/");
+            }
+        }
+        if (urlPath.equals("/") && session.getAttribute("state").equals("authorized")) {
+            String html = "<div>Hello, " + session.getAttribute("username") +
+                    " ! Твой user id: " +
+                    session.getAttribute("userId") +
+                    " </div>" +
+                    "<a href=\"/logout\">Logout</a>";
+            pageWriter.println(html);
+        }
+        if (urlPath.equals("/logout") && session.getAttribute("state").equals("authorized")){
+            session.setAttribute("state", "start");
+            httpServletResponse.sendRedirect("/");
+        }
+        HandleCount.incrementAndGet();
+    }
+
+    public void run(){
+        while(true){
+            System.out.println(HandleCount);
+            try{
+                Thread.sleep(10000);
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
